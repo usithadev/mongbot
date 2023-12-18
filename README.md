56790774b7067c71ee1d0d3411da9ac014e51dee
Usitha Indeewara <usithaindeewara181@gmail.com>
2023-12-18 22:02:36 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# Telegram bot for saveing and reading username and email
+
+This is a simple telegram bot that is used to save and read usernames and password.
+
+## Developer manual
+
+Environmental variables to setup:
+
+- `BOT_TOKEN` : The bot token got from the https://t.me/botfather (@BotFather)
+- `DB_URI` : The URI of the MongoDB database server
+
+### Installation
+
+```bash
+$ npm install
+```
+
+Or
+
+```bash
+$ npm install -D typescript @types/node
+$ npm install --save dotenv grammy mongoose
+```
+
+### Build and run commands:
+
+```bash
+$ npm start
+```
+
+Or:
+
+```bash
+$ npx tsc
+$ node index.js
+```
+
+## User manual
+
+Commands:
+
+- `/start` - Start the bot
+- `/save username email@example.com` - Save a record
+- `/getall` - See all data in the database
+
+## More info
+
+Developed by Usitha Indeewara. (Under GNU GPL v3)
+
+    Copyright (C) 2023  Usitha Indeewara
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ index.ts
@@ -0,0 +1,66 @@
+import { Bot } from "grammy";
+import { config as env } from "dotenv"; env();
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URI as string).then(() => {
+    console.log("Connected to the database");
+});
+
+mongoose.connection.on("error", (err) => console.log(err));
+
+interface IUser {
+    name: string;
+    email: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true }
+})
+
+const User = mongoose.model("User", userSchema);
+
+const bot = new Bot(process.env.BOT_TOKEN as string);
+
+bot.command("start", ctx => {
+    ctx.reply("Hello World!");
+});
+
+bot.command("save", async ctx => {
+    const item: string = ctx.match;
+    if (item.match(/(\w+) (\w*@\w+\.\w+)/gi)) {
+        const newuser = new User({
+            name: item.split(" ")[0],
+            email: item.split(" ")[1]
+        });
+
+        await newuser.save();
+
+        ctx.reply("Saved!");
+    } else {
+        ctx.reply("Invalid format\\! Please send in the format `/save username username@example\.com`", { parse_mode: "MarkdownV2" });
+    }
+});
+
+bot.command("getall", async ctx => {
+    const data = await User.find();
+    let str: string = "";
+    data.forEach(val => {
+        str += val.name + " :- " + val.email + "\n";
+    });
+
+    ctx.reply(str);
+});
+
+
+
+bot.start();
+console.log("Bot (may be) started");
+
+process.on("SIGINT", () => {
+    mongoose.disconnect().then(() => {
+        console.log("Disconnected the database");
+
+        process.exit(0);
+    })
+})
